42a277f7d14752fd4485a7fb8f028c56818a47ed
zgzhou <486768993@qq.com>
2024-02-19 21:09:10 +0800

    在类中添加爬虫和检测更新的部分，实现了对emdat更新数据的识别，还没实现更新数据的录入和对应波段信息的获取功能

--- emdatTools.py
+++ emdatTools.py
@@ -1,5 +1,8 @@
 import ee
 import os
+import requests
+import json
+import datetime
 import pandas as pd
 import pymysql
 import configparser
@@ -11,6 +14,7 @@ class Emdat():
         self.__cfg = configparser.ConfigParser()
         self.__BASE_DIR = os.path.dirname(os.path.abspath(__file__))
         self.__cfg.read(os.path.join(self.__BASE_DIR, 'emdatGee.ini'))
+        self.__emdat_account = dict(self.__cfg.items('emdat_account'))
         self.__conSet = dict(self.__cfg.items('mysql_connset'))
         self.__mysql_attribute = dict(self.__cfg.items('mysql_attribute'))
         self.__emdat_table = self.__mysql_attribute['emdat_table']
@@ -25,6 +29,89 @@ class Emdat():
             db=self.__conSet['database'],
             charset=self.__conSet['charset']
         )
+
+    def emdat_spider(self):
+        # current year
+        '''
+        l_year = datetime.datetime.today().year
+
+        output_path = 'data/test.xlsx'
+
+        # request url
+        url = 'https://public.emdat.be/graphql/'
+
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                                 'AppleWebKit/537.36 (KHTML, like Gecko) '
+                                 'Chrome/87.0.4280.67 Safari/537.36 Edg/87.0.664.47'}
+
+        # username password for login
+        login_data1 = {"operationName": "login",
+                       "variables": {"username": self.__emdat_account['emdat_username'],
+                                     "password": self.__emdat_account['emdat_password']},
+                       "query": "mutation login($username: String!, $password: String!) "
+                                "{\n  login(username: $username, password: $password)\n}"}
+
+        # login post step2 data
+        login_data2 = {"operationName": "self", "variables": {},
+                       "query": "query self {\n  self {\n    user_id\n    access {\n      "
+                                "access_case\n      login\n      public\n      entry\n      "
+                                "review\n      edit\n      admin\n    }\n    group {\n      "
+                                "user_group_id\n      user_group\n      need_license\n    }\n    "
+                                "roles {\n      role_ids\n      public\n    }\n    api_key\n  }\n}"}
+
+        # login post step 1
+        mys = requests.session()
+        mys.post(url, headers=headers, data=json.dumps(login_data1))
+
+        # login post step2
+        mys.post(url, headers=headers, data=json.dumps(login_data2))
+
+        # make search data
+        se_data = {"operationName": "xlsx", "variables": {
+            "form": {"period": [2017, l_year], "from": 2017, "include_hist": False, "classifications_tree": ["nat-*"],
+                     "classif": ["nat-*"]}},
+                   "query": "mutation xlsx($form: PublicForm) {\n  get_public_xlsx(form: $form) {\n    public_version\n    count\n    link\n  }\n}"}
+
+        # post search data get today's data url
+        td_resp = mys.post(url, headers=headers, data=json.dumps(se_data))
+        td_url = td_resp.json()['data']['get_public_xlsx']['link']
+
+        # download today's data
+        resp = mys.get(url=td_url, stream=True)
+        with open(output_path, mode='wb') as f:
+            for chunk in resp.iter_content(512):
+                f.write(chunk)
+        '''
+
+
+        # data update check
+        emdat_data = pd.read_excel('data/test.xlsx')
+        lastno_cur = emdat_data['DisNo.'].iloc[-1]
+
+        with open('data/lastNo.inf', 'r+', encoding='utf') as f:
+            lastno = f.readline()
+            if lastno != lastno_cur:
+                # update database
+                for i in range(-1, -len(emdat_data), -1):
+                    item = emdat_data.iloc[i]
+                    if item['DisNo.'] != lastno:
+                        if pd.notnull(item['Latitude']):
+                            engineInfo = '{0}+{1}://{2}:{3}@{4}:{5}/{6}?charset={7}'.format(
+                                self.__conSet['dialect'],
+                                self.__conSet['driver'],
+                                self.__conSet['username'],
+                                self.__conSet['password'],
+                                self.__conSet['host'],
+                                self.__conSet['port'],
+                                self.__conSet['database'],
+                                self.__conSet['charset']
+                            )
+                            conn = create_engine(engineInfo).connect()
+                    else:
+                        break
+                # f.write(lastno)
+            else:
+                pass
 
     def getEmdatFromMysql(self):
         '''
@@ -138,7 +225,8 @@ class Emdat():
         #         .filterDate(time[0], time[1]) \
         #         .map(mask_s2_clouds)
         else:
-            return "Wrong ImgCollection!"
+            print("Wrong ImgCollection!")
+            return
 
 
         # set roi
@@ -209,7 +297,8 @@ class Emdat():
 if __name__ == '__main__':
     emdat = Emdat()
     # print(emdat.getEmdatFromMysql())
-    emdat.initBandsInfo2DB()
+    emdat.emdat_spider()
+    # emdat.initBandsInfo2DB()
     # COPERNICUS/S2_SR_HARMONIZED
     # s2_bands = ['B1','B2','B3','B4','B5','B6','B7','B8','B8A','B9','B11','B12']
     # landsat8_bands = ['SR_B1', 'SR_B2', 'SR_B3', 'SR_B4', 'SR_B5', 'SR_B6', 'SR_B7']
